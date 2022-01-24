01d69e725e89540736ce48d00938e07206cfe7d0
Elvan H <elvanhd@gmail.com>
2022-01-24 20:07:07 +0700

    using the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipesComponent } from './recipes/recipes.component';
+import { RecipePageComponent } from './recipes/recipe-page/recipe-page.component';
 import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 
@@ -13,9 +13,9 @@ import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.co
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
+    RecipePageComponent,
     RecipeListComponent,
-    RecipeDetailsComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
