d4c15048295e51bd00ecce02152470844d6501ac
Elvan H <elvanhd@gmail.com>
2022-02-06 17:58:56 +0700

    adding content to the recipes component

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe({
+      name: 'Schnitzel',
+      description: 'Very tasty',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+    }),
+    new Recipe({
+      name: 'Big Fat Burger',
+      description: 'What else you need to say?',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+    }),
+    new Recipe({
+      name: 'Spaghetti',
+      description: 'The best spagetti ever',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    }),
+  ];
 
   constructor() {}
 
